6b240f1284f2294c5d6d6fe2c287f002d97a29b6
Rut Navarro Hernandez <ruuthernandez14@gmail.com>
2021-03-11 16:32:59 +0100

    Añado barra navegacion y menu slider

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+function openNav() {
+    document.getElementById('miMenu').style.width = '100%';
+    document.getElementById('miMenu').style.height = '100%';
+
+}
+
+function closeNav() {
+    document.getElementById('miMenu').style.width = '0';
+    document.getElementById('miMenu').style.height = '0';
+}
