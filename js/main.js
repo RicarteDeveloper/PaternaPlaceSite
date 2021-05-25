8b58a0de8625c43ad7360bc350a35c78bdb06f81
Rut Navarro Hernandez <ruuthernandez14@gmail.com>
2021-05-25 12:50:49 +0200

    Añadido loader

--- js/main.js
+++ js/main.js
@@ -11,27 +11,29 @@ const LNG = '-0.442983'; // MapaConfig
 
 const ZOOM = 16; // MapaConfig
 
-var navBar = $("#miMenu"); //barra navegacion
+var navBar = document.getElementById('miMenu');//barra navegacion
 
-var iconMenu = $(".icon-menu"); //menu mobile icon
+var loader = document.querySelector('.fondo-loading'); //loader
 
-var closeMenu = $(".btn-close"); //boton cerrar menu movil
+var iconMenu = document.querySelector('.icon-menu'); //menu mobile icon
 
-var modal = $("#myModal"); //modal
+var closeMenu = document.querySelector('.btn-close'); //boton cerrar menu movil
 
-var btn = $("#mapa"); //Boton que abre el modal
+var modal = document.getElementById('myModal');//modal
 
-var closeModal = $(".close")[0]; //Elemento que cierra el modal
+var btn = document.getElementById('mapa'); //Boton que abre el modal
 
-var modalFilter = $("#myModalFilter"); //modal filtros categorias
+var closeModal = document.querySelector('.close-modal'); //Elemento que cierra el modal
 
-var btnFilter = $("#btnFilter"); //Boton que abre el modal filtros categorias
+var modalFilter = document.getElementById('myModalFilter');//modal filtros categorias
 
-var closeFilter = $(".close-filter")[0]; //Elemento que cierra el modal filtros categorias
+var btnFilter = document.getElementById('btnFilter'); //Boton que abre el modal filtros categorias
 
-var cardBlanco = $(".card-blanco"); //card slide (movil)
+var closeFilter = document.querySelector('.close-filter'); //Elemento que cierra el modal filtros categorias
 
-var closeBar = $(".close-bar"); //boton que cierra card blanco (movil)
+var cardBlanco = document.querySelector('.card-blanco'); //card slide (movil)
+
+var closeBar = document.querySelector('.close-bar'); //boton que cierra card blanco (movil)
 
 // Variables locales
 var categoriaList = [];
@@ -40,148 +42,91 @@ var comerciosList = [];
 
 var map = L.map('mapa', { zoomControl: false }).setView([LAT, LNG], ZOOM);
 
-
-// **** EVENTOS **** //
-$(document).ready(function () {
-    //creamos los eventos
-    $(closeMenu).on('click', closeNav);
-    $(iconMenu).on('click', openNav);
-    $(closeModal).on('click', cerrarModal);
-    $(btnFilter).on('click', abrirFiltros);
-    $(closeFilter).on('click', cerrarFiltros);
-    $(closeBar).on('click', cerrarCardBlanco);
-});
-
+closeMenu.addEventListener('click', () => closeNav());
+iconMenu.addEventListener('click', () => openNav());
+closeModal.addEventListener('click', () => cerrarModal());
+btnFilter.addEventListener('click', () => abrirFiltros());
+closeFilter.addEventListener('click', () => cerrarFiltros());
+closeBar.addEventListener('click', () => cerrarCardBlanco());
 
 document.addEventListener('DOMContentLoaded', async function () {
     console.log('Hola Desde ContentLoaded');
-
-
-
-    // **** OBJETOS **** //
-
-    //mapa
-    //hacemos una variable para customizar y pasarle los datos de nuestro marker 
-    var iconHosteleria = new L.Icon({    //variable para customizar y pasarle los datos de nuestro marker 
-        iconUrl: '../img/markers/hosteleria.png',
-        iconSize: [45, 45],
-        iconAnchor: [25, 45]
-    });
-
-    /*     var comercios = [//listado comercios
-            { id: 1, lat: '39.508229', lng: '-0.442983', icon: iconHosteleria, visitas: 3 },
-            { id: 2, lat: '39.507476', lng: '-0.445624', icon: iconHosteleria, visitas: 3 },
-            { id: 3, lat: '39.506127', lng: '-0.444249', icon: iconModa, visitas: 3 },
-            { id: 4, lat: '39.505854', lng: '-0.442371', icon: iconHosteleria, visitas: 3 },
-            { id: 5, lat: '39.506201', lng: '-0.446255', icon: iconModa, visitas: 3 },
-        ]; */
-
-
     //agregamos el mapa al div mapa que hemos creado
     L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
         subdomains: 'abcd',
         maxZoom: 19
     }).addTo(map);
-
-
-
-    //recorremos el array de comercios 
-    /*         for (let i = 0; i < comercios.length; i++) {
-                const comercio = comercios[i]; //obtenemos cada comercio
-                //le añadimos lat - lng el icon y lo añadimos al mapa
-                L.marker([comercio.lat, comercio.lng], { icon: comercio.icon }).addTo(map).on('mouseup', function (event) {
-                    onClickMarker(comercio);
-                });
-            } */
-
-
-    // TODO --> LLamar API para llenar la lista de comercios.
     // llamanos al API
     getComercios(API_Comercios);
-    async function getComercios(url) {
-        const resp = await fetch(url);
-        const data = await resp.json();
-        //console.log(data); //me devuleve lista de objetos con los comercios
-        datosComercios(data);
-    }
+}); //DOM CONTENT LOADED 
 
-    function datosComercios(comerciosApi) { //nos devuelve los datos de los comercios
+async function getComercios(url) {
+    const resp = await fetch(url);
+    const data = await resp.json();
+    //esconder loader
+    closeLoader(500);
+    //console.log(data); //me devuleve lista de objetos con los comercios
+    datosComercios(data);
+}
 
-        comerciosApi.forEach((comercioApi) => {
-            const { nombre, imagen, descripcion, latitud, longitud, horario, telefono, link_fb, link_ig, categoria } = comercioApi;
-
-            //mostrar markadores en latitud y longitud correspondiente a cada comercio
-            L.marker(
-                [latitud, longitud],
-                { icon: new L.Icon({
+function datosComercios(comerciosApi) { //nos devuelve los datos de los comercios
+    comerciosApi.forEach((comercioApi) => {
+        const { nombre, imagen, descripcion, latitud, longitud, horario, telefono, link_fb, link_ig, categoria } = comercioApi;
+        //mostrar markadores en latitud y longitud correspondiente a cada comercio
+        L.marker(
+            [latitud, longitud],
+            {
+                icon: new L.Icon({
                     iconUrl: `../img/markers/${categoria.marker}.png`,
                     iconSize: [45, 45],
                     iconAnchor: [25, 45]
-                })}
-            ).addTo(map).on('mouseup', function (event) {
-                onClickMarker(comercioApi);
-
-            });
-
-
+                })
+            }
+        ).addTo(map).on('mouseup', function (event) {
+            onClickMarker(comercioApi);
         });
-
-    }
-
-
-}); //DOM CONTENT LOADED 
-
-
-
-// **** FUNCIONES **** //
+    });
+}
 
 function cerrarModal() { //cerrar el modal
-    $(modal).css({ 'display': 'none' });
+    modal.style.display = 'none';
 }
 function abrirFiltros() { //cerrar el modal
-    $(modalFilter).css({ 'display': 'block' });
-    $(cardBlanco).css({ 'bottom': '-1000px' });
+    modalFilter.style.display = 'block';
+    cardBlanco.style.bottom = '-1000px';
     console.log('click en boton filter');
 }
 function cerrarFiltros() { //cerrar el modal filtros categorias
-    $(modalFilter).css({ 'display': 'none' });
-
+    modalFilter.style.display = 'none';
 }
 function cerrarCardBlanco() {     //cuando demos click en la barra se cierra el card blanco
-    $(cardBlanco).css({ 'bottom': '-1000px' });
-    return false;
-}
-function cerrarCardBlanco() {     //cuando demos click en la barra se cierra el card blanco
-    $(cardBlanco).css({ 'bottom': '-1000px' });
+    cardBlanco.style.bottom = '-1000px';
     return false;
 }
 function openNav() { //abre menu del movil
-    $(navBar).css({ 'width': '100%' });
-    $(navBar).css({ 'height': '100%' });
+    navBar.style.width = '100%';
+    navBar.style.height = '100%';
 }
 function closeNav() { //cierra menu del movil
-    $(navBar).css({ 'width': '0' });
-    $(navBar).css({ 'height': '0' });
+    navBar.style.width = '0';
+    navBar.style.height = '0';
 }
 
 function onClickMarker(comercio) { //cuando damos click al marker del comercio comprueba si estas en movil o en web
     console.log(comercio);
-    // var altura = $(document).height();
-    var anchura = $(document).width();
+    var anchura = screen.width;
     if (anchura <= 700) {
         contenido.innerHTML = '';
         rellenarComercioMv(comercio);
-        $(cardBlanco).css({ 'bottom': '0px' });
+        cardBlanco.style.bottom = '0px';
     } else {
         contenidoWeb.innerHTML = '';
         rellenarComercioWeb(comercio);
-        $(modal).css({ 'display': 'block' });
+        modal.style.display = 'block';
     }
-    // TODO --> Pintar información del comercio
-
-
 }
+
 function rellenarComercioMv(datosComercio) {
     const comercioEl = document.createElement('div');
     comercioEl.classList.add('card');
@@ -343,4 +288,12 @@ function rellenarComercioWeb(datosComercio) {
     `;
     contenidoWeb.appendChild(comercioWebEl);
 
+}
+function closeLoader(time) {
+    setTimeout(() => {
+        loader.style.display = 'none';
+    }, time);
+}
+function openLoader() {
+    loader.style.display = 'block';
 }
